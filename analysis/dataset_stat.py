fd5c35f8b9a31f2a6c045e1cf88a6704b1800349
Damith Premasiri <damithpremasiri@gmail.com>
2024-12-24 03:34:23 +0000

    enhance for cases without citations

--- analysis/dataset_stat.py
+++ analysis/dataset_stat.py
@@ -9,6 +9,7 @@ with open(dataset, 'r', encoding="utf-8") as f:
 
 case_wise_total_citation_counts = []
 no_citation_cases = 0
+print(f'Total number of cases {len(dataset)}')
 for data in tqdm(dataset):
     found = False
     keys = data['sequence']
@@ -25,7 +26,11 @@ for data in tqdm(dataset):
     case_wise_total_citation_counts.append(total_citations)
 
 # Create the histogram
+case_wise_total_citation_counts = [x for x in case_wise_total_citation_counts if x != 0]
 counts, bins, patches = plt.hist(case_wise_total_citation_counts, bins=5, edgecolor='black', alpha=0.7)
+
+print(f'cases without citations {no_citation_cases}')
+print(f'cases with citations {len(case_wise_total_citation_counts)}')
 
 # Add labels and title
 plt.title("Histogram of Data with Frequencies")
